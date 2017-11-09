779a77755fe7a7b58de9f4df3eea29d7785563f8
rsi2940 <rishi2940@gmail.com>
2017-11-08 19:38:44 -0500

    added heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = "app";
-  arr = ["a", "b", "c"];
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  constructor() {}
+  ngOnInit() {}
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+  // encapsulation: ViewEncapsulation.None
+})
+export class HeroesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
